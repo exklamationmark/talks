8df3170156aaa70be1c10c5eb4eaa0fd073a4a76
¨Huu Khiem (Mark)¨ <tonghuukhiem@gmail.com>
2015-04-14 12:56:41 +0800

    server with memory creation

---
+++ singapore_gophers_apr_2015/server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	`encoding/json`
+	`log`
+	`net/http`
+	_ `net/http/pprof`
+	`time`
+)
+
+type Gopher struct {
+	BirthDay int64 `json:"birth_date"`
+}
+
+var (
+	errNoGopher = []byte(`{"failed":"no gopher for you"}`)
+)
+
+func main() {
+	http.HandleFunc(`/`, handler)
+	log.Fatal(http.ListenAndServe(`:9090`, nil))
+}
+
+func handler(resp http.ResponseWriter, req *http.Request) {
+	body, err := json.Marshal(NewGopher())
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write(errNoGopher)
+		return
+	}
+
+	resp.WriteHeader(http.StatusOK)
+	resp.Write(body)
+}
+
+func NewGopher() *Gopher {
+	return &Gopher{time.Now().Unix()}
+}
